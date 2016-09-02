76907eb5e96a6df7a690512905f3929010166663
david Karchmer <dkarchmer@gmail.com>
2016-09-02 15:14:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Strato Python API Package
+
+A python library for interacting with [Strato](https://strato.arch-iot.com) Rest API
+
+## Installation
+
+```
+pip install pystrato
+```
+
+Package is based on https://github.com/samgiles/slumber
+
+## Requirements
+
+pystrato requires the following modules.
+
+Python 2.7+ or 3.4+
+requests
+
+## Copyright and license
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pystrato',
+      version='0.1.0',
+      description='Python client for strato.arch-iot.com',
+      url='https://github.com/iotile/strato_python_api',
+      author='David Karchmer',
+      author_email='david@arch-iot.com',
+      license='MIT',
+      packages=['pystrato'],
+      install_requires=[
+        'requests',
+      ],
+      zip_safe=False)
