b110fc60c02a2e4ddda227320f3e42e3f2e1e023
david Karchmer <dkarchmer@gmail.com>
2016-10-08 11:09:05 -0700

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pystrato',
-      version='0.1.0',
+      version='0.2.0',
       description='Python client for strato.arch-iot.com',
       url='https://github.com/iotile/strato_python_api',
       author='David Karchmer',
