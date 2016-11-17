7bc910494f0ef53c9edca9da97aee58f2cfd5215
david Karchmer <dkarchmer@gmail.com>
2016-11-17 12:38:26 -0800

    Fix setup.py to include sub-packages

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
 setup(name='pystrato',
-      version='0.3.0',
+      version='0.3.1',
       description='Python client for https://iotile.cloud',
       url='https://github.com/iotile/strato_python_api',
       author='David Karchmer',
       author_email='david@arch-iot.com',
       license='MIT',
-      packages=['pystrato'],
+      packages=[
+            'pystrato',
+            'pystrato.api',
+            'pystrato.stream'
+      ],
       install_requires=[
         'requests',
       ],
