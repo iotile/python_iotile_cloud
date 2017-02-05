bfe53783198f10f6a9f411aca986763b1bc07eb1
Tim Burke <timburke@alumni.rice.edu>
2017-02-04 17:41:28 -0800

    Prepare for CI setup

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup
+import version
 
 setup(name='iotile_cloud',
-      version='0.4.0',
-      description='Python client for https://iotile.cloud',
-      url='https://github.com/iotile/python_iotile_cloud',
-      author='David Karchmer',
-      author_email='david@arch-iot.com',
-      license='MIT',
-      packages=[
-            'iotile_cloud',
-            'iotile_cloud.api',
-            'iotile_cloud.stream'
-      ],
-      install_requires=[
-        'requests',
-      ],
-      zip_safe=False)
+    version=version.version,
+    description='Python client for https://iotile.cloud',
+    url='https://github.com/iotile/python_iotile_cloud',
+    author='David Karchmer',
+    author_email='david@arch-iot.com',
+    license='MIT',
+    packages=[
+        'iotile_cloud',
+        'iotile_cloud.api',
+        'iotile_cloud.stream'
+    ],
+    install_requires=[
+        'requests'
+    ],
+    keywords=["iotile", "arch", "embedded", "hardware"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    zip_safe=False)
